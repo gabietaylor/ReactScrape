e43d01df9b392cca128757ba421d911f205472f7
gabietaylor <gabie.taylor@yahoo.com>
2017-07-24 16:45:22 -0400

    required npms in server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const Article = require('./models/Article.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Run Morgan for Logging
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.use(express.static('./public'));
