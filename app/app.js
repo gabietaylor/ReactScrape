db2da18c2a862c4232f126e81f62639a547b1e29
gabietaylor <gabie.taylor@yahoo.com>
2017-07-24 17:25:52 -0400

    main comp rendered

---
+++ app/app.js
@@ -0,0 +1,8 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+// The Main Component
+const Main = require('./components/Main')
+
+// render Main
+ReactDOM.render(<Main />, document.getElementById('app'));
