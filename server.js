75e59d6caa19ff5dc1314bfe44c4433912087679
gabietaylor <gabie.taylor@yahoo.com>
2017-07-24 16:48:08 -0400

    mongo connected

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// NPMs
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
@@ -8,7 +9,7 @@ const Article = require('./models/Article.js');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Run Morgan for Logging
+// Morgan for Logging
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,3 +17,19 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 app.use(express.static('./public'));
+
+// localhost
+mongoose.connect('mongodb://localhost/nytreact');
+// Use for Heroku Deployment
+// mongoose.connect('mongodb://heroku_t2wjvmwp:knd9pgbjt992ep3t6fm18ca2vd@ds149268.mlab.com:49268/heroku_t2wjvmwp');
+
+
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+  console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function () {
+  console.log('Mongoose connection successful.');
+});
